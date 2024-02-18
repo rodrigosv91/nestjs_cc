3e72824fdfc25e2fa3c0ea8c5c51410ba6f062ff
rodrigo <rodrigosv91@gmail.com>
2024-02-18 00:25:07 -0300

    add: create get with param

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get all Items';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `Item ${id}`;
   }
 }

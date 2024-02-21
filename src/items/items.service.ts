d36fcc22b69267e5bada12767115e8cc6882b3d1
rodrigo <rodrigosv91@gmail.com>
2024-02-20 22:48:34 -0300

    add: create update and delete methods

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './interfaces/item.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ItemsService {
-  private readonly items: Item[] = [
-    {
-      id: '32424234',
-      name: 'Item one',
-      qty: 100,
-      description: 'Item one description',
-    },
-    {
-      id: '5566544',
-      name: 'Item two',
-      qty: 200,
-      description: 'Item two description',
-    },
-  ];
+  constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) {}
 
-  findAll(): Item[] {
-    return this.items;
+  async findAll(): Promise<Item[]> {
+    return await this.itemModel.find();
   }
 
-  findOne(id: string): Item {
-    return this.items.find((item) => item.id === id);
+  async findOne(id: string): Promise<Item> {
+    return await this.itemModel.findOne({ _id: id });
+  }
+
+  async create(item: Item): Promise<Item> {
+    const newItem = new this.itemModel(item);
+    return await newItem.save();
+  }
+
+  async delete(id: string): Promise<Item> {
+    return await this.itemModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, item: Item): Promise<Item> {
+    return await this.itemModel.findByIdAndUpdate(id, item, { new: true });
   }
 }
